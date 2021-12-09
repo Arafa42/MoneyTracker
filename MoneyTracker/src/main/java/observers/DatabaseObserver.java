010c64fdb39d99ab4f05bbe48242a84a7f94fca1
Ayoub <s102331@ap.be>
2021-12-09 15:40:47 +0100

    Jens

--- MoneyTracker/src/main/java/observers/DatabaseObserver.java
+++ MoneyTracker/src/main/java/observers/DatabaseObserver.java
@@ -4,6 +4,8 @@ import java.util.Observable;
 import java.util.Observer;
 
 public class DatabaseObserver implements Observer {
+    // Jens heeft gekeken en vondt het in orde omdit tegebruiken!
+    // propertychangelistener
     @Override
     public void update(Observable o, Object arg) {
         System.out.println("DB UPDATED");
