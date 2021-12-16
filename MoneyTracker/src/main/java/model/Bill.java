4074da0b03a95ddd22e5d39fedc227ea8b1f18d2
Arafa42 <s107661@ap.be>
2021-12-16 11:17:59 +0100

    bill hashmap created

--- MoneyTracker/src/main/java/model/Bill.java
+++ MoneyTracker/src/main/java/model/Bill.java
@@ -1,29 +1,27 @@
 package model;
 
+import java.util.HashMap;
+import java.util.List;
+
 public class Bill {
 
     private int id;
-    private String name;
-    private Double amountToPay;
+    private List<HashMap<String,Double>> amountToReceive;
 
-    public Bill(String name, Double amountToPay) {
-        this.name = name;
-        this.amountToPay = amountToPay;
+
+    public Bill(List<HashMap<String,Double>> amountToReceive) {
+        this.amountToReceive = amountToReceive;
     }
 
     public int getId() { return id; }
     public void setId(int id) { this.id = id; }
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-    public Double getAmountToPay() { return amountToPay; }
-    public void setAmountToPay(Double amountToPay) { this.amountToPay = amountToPay; }
+
 
     @Override
     public String toString() {
         return "Bill{" +
                 "id=" + id +
-                ", name='" + name + '\'' +
-                ", amountToPay=" + amountToPay +
+                ", amountToReceive=" + amountToReceive +
                 '}';
     }
 
