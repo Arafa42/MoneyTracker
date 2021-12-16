4dcca49a3921394372ceecc91fd719a0116f1709
Arafa42 <s107661@ap.be>
2021-12-16 18:34:04 +0100

    interface getuserbyname added

--- MoneyTracker/src/main/java/controller/IUser.java
+++ MoneyTracker/src/main/java/controller/IUser.java
@@ -6,5 +6,6 @@ public interface IUser {
     void addUser(User u);
     void removeUser(User u);
     List<User> getAllUsersSortedById();
+    User getUserByName(String name);
     void deleteUserById(int id);
 }
