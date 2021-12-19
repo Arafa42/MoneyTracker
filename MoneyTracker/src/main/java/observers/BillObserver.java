9a546a35d69369a9225bc5e3b7b88c045bebcf11
Ayoub-cmd707 <45266255+Ayoub-cmd707@users.noreply.github.com>
2021-12-19 23:37:42 +0100

    fix

--- MoneyTracker/src/main/java/observers/BillObserver.java
+++ MoneyTracker/src/main/java/observers/BillObserver.java
@@ -1,5 +1,6 @@
 package observers;
 
+import database.BillsDB;
 import database.PersonsDB;
 import model.Bill;
 import model.User;
@@ -10,14 +11,16 @@ import java.beans.PropertyChangeListener;
 import java.util.Observable;
 import java.util.Observer;
 // Jens heeft gekeken en vondt het in orde omdit tegebruiken!
-// propertychangelistener
+    // propertychangelistener
 public class BillObserver implements PropertyChangeListener {
     @Override
     public void propertyChange(PropertyChangeEvent evt) {
         Bill bill = (Bill) evt.getNewValue();
         String owner =  bill.getOwnerName();
 
+        RegisterEntry registerEntry = BillsDB.getInstance().getEntry((Bill) evt.getNewValue());
 
+        System.out.println("Name: "+ owner +", and entry: "+ registerEntry);
 
     }
 
